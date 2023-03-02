0a0f715d5fab5d23f60e44db10bcef8899ca04b4
eliudsaj <eliudkaranja5@gmail.com>
2023-03-02 05:24:42 -0800

    hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer to store result
- * @size_r: buffer size
- * Return: pointer to result, or 0 if result cannot be stored in r
+ * infinite_add - add two string numbers
+ * @n1: string number to add to n2
+ * @n2: string number to add to n1
+ * @r: buffer to store the sum
+ * @size_r: size of buffer
+ * return: pointer to sum or 0 if it does not fit in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int len1 = 0, len2 = 0, lenS = 0;
-
-        while (*(n1 + len1++))
-                ;
-        while (*(n2 + len2++))
-                ;
-        printf("%d, %d\n", len1, len2);
-        return (r);
+	int len1 = 0, len2 = 0, lenS = 0;
+	
+	while (*(n1 + len1++))
+		;
+	while (*(n2 + len2++))
+		;
+	printf("%d, %d\n", len1, len2);
+	return (r);
 }
