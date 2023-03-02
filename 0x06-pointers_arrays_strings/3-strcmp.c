8680d7229152302b9cdd4fef13aac25dca0def78
eliudsaj <eliudkaranja5@gmail.com>
2023-03-01 23:09:09 -0800

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,14 @@
  * Return: an integer less than, equal to, or greater than zero if s1 is found,
  * respectively, to be less than, to match, or be greater than s2.
  */
-int _strcmp(const char *s1, const char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
-		s1++, s2++;
+	int i;
 
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
