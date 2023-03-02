c4cfade5e71bb2a7bf711028ff7fba2d78424808
eliudsaj <eliudkaranja5@gmail.com>
2023-03-02 05:35:28 -0800

    hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,21 +1,52 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * infinite_add - add two string numbers
- * @n1: string number to add to n2
- * @n2: string number to add to n1
- * @r: buffer to store the sum
- * @size_r: size of buffer
- * return: pointer to sum or 0 if it does not fit in r
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store result
+ * @size_r: buffer size
+ * Return: pointer to result, or 0 if result cannot be stored in r
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, lenS = 0;
+	int len1, len2, len_res, carry, sum, i, j;
+
 	
-	while (*(n1 + len1++))
+	for (len1 = 0; n1[len1] != '\0'; len1++)
 		;
-	while (*(n2 + len2++))
+	for (len2 = 0; n2[len2] != '\0'; len2++)
 		;
-	printf("%d, %d\n", len1, len2);
+
+	
+	if (len1 >= size_r || len2 >= size_r || len1 + 1 >= size_r || len2 + 1 >= size_r)
+		return (0);
+
+	
+	i = len1 - 1;
+	j = len2 - 1;
+	carry = 0;
+	len_res = 0;
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		r[len_res++] = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+
+	
+	for (i = 0, j = len_res - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+	}
+
+	r[len_res] = '\0'; 
 	return (r);
 }
