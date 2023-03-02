c17ada9898ec46f771a1946354dde055a9ff320b
eliudsaj <eliudkaranja5@gmail.com>
2023-03-02 05:21:06 -0800

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -1,21 +1,59 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer to store result
- * @size_r: buffer size
- * Return: pointer to result, or 0 if result cannot be stored in r
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int len1 = 0, len2 = 0, lenS = 0;
-	
-	while (*(n1 + len1++))
-		;
-	while (*(n2 + len2++))
-		;
-	printf("%d, %d\n", len1, len2);
-	return (r);
+        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+        char r[100];
+        char r2[10];
+        char r3[11];
+        char *res;
+
+        res = infinite_add(n, m, r, 100);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "1234567890";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "999999999";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        res = infinite_add(n, m, r3, 11);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        return (0);
 }
