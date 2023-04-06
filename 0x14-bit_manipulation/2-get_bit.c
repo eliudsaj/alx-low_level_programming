c5aa0601702d0bc07499b9d0a977bf9032f4f08f
eliudsaj <eliudkaranja5@gmail.com>
2023-04-06 06:30:44 -0700

    edi for betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,15 @@
 /**
- * get_bit - check the bit at an index
- * @n: number to check bits
- * @index: index to check
- * Return: bit at the index or -1
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to check the bit from
+ * @index: index of the bit to check
+ *
+ * Return: value of the bit at index index, or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if 9index >= sizeof(unsigned long int) * 8)
-		return (-1);
+	unsigned int bits = sizeof(int) * 8, bit = 1;
 
-	return ((n >> index) & 1);
+	if (index >= bits)
+		return (-1);
+	return ((bit << index & n) ? 1 : 0);
 }
